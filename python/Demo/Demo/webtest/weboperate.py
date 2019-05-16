f932169e5c72bd870abbfc8c485dd7cd5d4ee3d0
sunwenhao <sunwenhao@thunisoft.com>
2019-05-16 15:01:01 +0800

    项目结构修改，增加python对页面操作文件

---
+++ python/Demo/Demo/webtest/weboperate.py
@@ -0,0 +1,6 @@
+#!/usr/python/bin
+
+from selenium import webdriver
+
+driver=webdriver.Chrome(executable_path ="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+driver.get("https://www.baidu.com")
