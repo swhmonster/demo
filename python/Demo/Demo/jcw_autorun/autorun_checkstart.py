0f3d188c93e4f621028dae070b2b560adc1353f0
sunwenhao <sunwenhao@thunisoft.com>
2019-05-22 14:09:53 +0800

    批量启动，check更新

--- python/Demo/Demo/jcw_autorun/autorun_checkstart.py
+++ python/Demo/Demo/jcw_autorun/autorun_checkstart.py
@@ -4,8 +4,19 @@
 import os
 
 portSet = set()
-portSet.add("3939")  # xspch
-portSet.add("2019")  # zjjdapi
+portSet.add("3939")  # 1 xspch
+portSet.add("2019")  # 2 zjjdapi
+portSet.add("2022")  # 3 agapi
+portSet.add("2021")  # 4 detail
+portSet.add("2024")  # 5 filesys
+portSet.add("2300")  # 6 dept-prop
+portSet.add("2025")  # 7 serial
+portSet.add("2280")  # 8 xxtx
+portSet.add("2020")  # 9 agxt
+portSet.add("81")  # 9 ajsl
+portSet.add("82")  # 9 scdc
+portSet.add("83")  # 9 zjjd
+
 counter = 1
 for port in portSet:
     pid = \
@@ -14,5 +25,5 @@ for port in portSet:
     if (pid == ""):
         print("未启动port：%s" % (port))
     else:
-        print("已启动port,pid：%s,%s；计数：%d" % (port, pid, counter))
+        print("已启动port：%s ；pid：%s；计数：%d" % (port, pid, counter))
         counter += 1
