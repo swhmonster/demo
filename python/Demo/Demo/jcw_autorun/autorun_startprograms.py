20af4f3ccfea90a7458cf52b23b2ebf517adea73
sunwenhao <sunwenhao@thunisoft.com>
2019-06-06 13:44:06 +0800

    启停脚本，日志输出问题修改

--- python/Demo/Demo/jcw_autorun/autorun_startprograms.py
+++ python/Demo/Demo/jcw_autorun/autorun_startprograms.py
@@ -22,8 +22,18 @@ def get_pid(port):
 
 
 def run_program(jdk_path, jar_name):
-    os.system("nohup %s -jar %s &" % (jdk_path + "java", jar_name))
-    print("正在启动：%s" % (jar_name))
+    jar_path = ""
+    list = jar_name.split("/")
+    for i in range(0, list.__len__() - 1):
+        jar_path+=(list[i]+"/")
+    if (jdk_path == ''):
+        print(jar_path)
+        os.system("cd %s && nohup java -jar %s > file 2>&1 &" % (jar_path, jar_name))
+        print("正在启动：%s" % (jar_name))
+    else:
+        print(jar_path)
+        os.system("cd %s && nohup %s -jar %s > file 2>&1 &" % (jar_path,jdk_path, jar_name))
+        print("正在启动：%s" % (jar_name))
 
 
 def run_tomcat(sh_name):
@@ -37,7 +47,6 @@ def run_tas(sh_name):
 
 
 def run_program_priority(jdk_path, list, priorityDict):
-    i = 0
     for i in range(0, len(list)):
         run_program(jdk_path, list[i])
         print(list[i] + "starting...")
@@ -50,21 +59,18 @@ def run_program_priority(jdk_path, list, priorityDict):
 def run_self_define(jdk_path, gpus, proMap, sysMap):
     if (gpus.find("tomcat") == 0):
         tomcatlist = str(gpus).split(",")
-        j = 0
         for j in range(0, len(tomcatlist)):
             portlist = str(tomcatlist[j]).split(":")
             kill_process(get_pid(portlist[1]))
             run_tomcat(proMap[portlist[1]])
     elif (gpus.find("tas") == 0):
         taslist = str(gpus).split(",")
-        j = 0
         for j in range(0, len(taslist)):
             portlist = str(taslist[j]).split(":")
             kill_process(get_pid(portlist[1]))
             run_tas(proMap[portlist[1]])
     else:
         list = str(gpus).split(",")
-        j = 0
         for j in range(0, len(list)):
             kill_process(get_pid(sysMap[list[j]]))
             run_program(jdk_path, proMap[sysMap[list[j]]])
