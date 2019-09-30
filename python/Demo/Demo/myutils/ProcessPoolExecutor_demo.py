6811bc15f5427c710c3ab5882c89089c94372fdb
sunwenhao <sunwenhao@thunisoft.com>
2019-09-30 17:12:32 +0800

    线程并发与进程并发

---
+++ python/Demo/Demo/myutils/ProcessPoolExecutor_demo.py
@@ -0,0 +1,34 @@
+import concurrent.futures
+import math
+
+# ProcessPoolExecutor 是 Executor 的子类，它使用进程池来实现异步执行调用。
+# ProcessPoolExecutor 使用 multiprocessing 回避 Global Interpreter Lock 但也意味着只可以处理和返回可序列化的对象。
+
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419]
+
+
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+
+
+if __name__ == '__main__':
+    main()
